1382db0bf5ea5f104c12fa2d86bce66cdfcf4b39
Jeroen Wolff <jeroen.wolff@gmail.com>
2018-06-26 23:37:42 +0200

    initial add

---
+++ vertx-demo/src/main/java/nl/jr00n/MainVerticle.java
@@ -0,0 +1,19 @@
+package nl.jr00n;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+
+public class MainVerticle extends AbstractVerticle {
+    final String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
+    
+    @Override
+    public void start() {
+        Router router = Router.router(vertx);
+        router.get("/hello").handler(request -> {
+            request.response().end("Namaste from Vert.x! " + new java.util.Date() + " on " + hostname);
+        });
+        vertx.createHttpServer().requestHandler(router::accept)
+            .listen(8080);
+    }
+
+}
